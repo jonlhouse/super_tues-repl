d898f057ddc35a8fc6a92bbe7ddec79ff8f2ff52
John House <jonlhouse@gmail.com>
2014-01-30 21:40:00 -0600

    repl runnable

---
+++ lib/super_tues/repl/all.rb
@@ -0,0 +1,2 @@
+require "super_tues/repl/version"
+require "super_tues/repl/runner"
---
+++ lib/super_tues/repl/runner.rb
@@ -0,0 +1,12 @@
+module SuperTues
+  module Repl
+    class Game
+
+      # Main entry point for the SuperTues command line REPL.
+      # 
+      def play
+        puts "Welcome to SuperTues!!"
+      end
+    end    
+  end
+end
